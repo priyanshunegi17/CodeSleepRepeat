1d03d2d9131b585a744c3ce9a4cea57ce6d3f006
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-13 18:16:10 +0530

    Added solution - LeetHub

---
+++ K-th missing element - GFG/kth-missing-element.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+//User function Template for Java
+
+            
+
+class Complete{
+    
+  
+    int KthMissingElement(int a[], int n, int k)
+    {
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+        }
+        int mac = a[n-1];
+        int act = (mac*(mac+1))/2;
+        if(sum==act){
+            return -1;
+        }
+        int c=0;
+        int b=a[0];
+        for(int i=0;i<n;i++){
+            if(a[i]!=b){
+                c++;
+                if(c==k){
+                    return b;
+                }
+                i--;
+                b++;
+            }
+            else{
+                b++;
+            }
+        }
+        return -1;
+    }
+  
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Complete obj = new Complete();
+		   
+		    int res = obj.KthMissingElement(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		    
+		    
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
