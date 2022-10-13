905a37ca683bd985612370b6e0bb5a3f527013d2
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-13 12:06:34 +0530

    Added solution - LeetHub

--- Koko Eating Bananas - GFG/koko-eating-bananas.java
+++ Koko Eating Bananas - GFG/koko-eating-bananas.java
@@ -23,35 +23,50 @@ class GFG {
 
 // } Driver Code Ends
 
+// https://practice.geeksforgeeks.org/problems/koko-eating-bananas/1
+// Code with Alisha...
 
 class Solution {
     
-    static int calcTime(int piles[],int speed){
-        int time=0;
-        for(int i=0;i<piles.length;i++){
-            time+=(piles[i]/speed)+((piles[i]%speed)!=0?1:0);
+    // for a given speed, calculate how much time it'll take to eat all the bananas...
+    static int calcTime(int[] piles,int speed){
+        int n = piles.length;
+        int count=0;
+        // we have to take the ceiling values... i.e 7(bananas)/5(speed) should give 2.. not 1.. that's why
+        for(int i=0;i<n;i++){
+            count+=(int)Math.ceil(piles[i]/(double)speed);
         }
-        return time;
+        return count;
     }
     
     public static int Solve(int N, int[] piles, int H) {
-        Arrays.sort(piles);
-        int low = 1, high =piles[N-1];
         
-        int ans=0;
+        // sort the array... 
+        Arrays.sort(piles);
+        int last = piles[N-1];
+        // if array size is equals to the Hours.. then return the max element present in array..
+        if(N==H){
+            return last;
+        }
+        
+        // initialize the values... 
+        int min=0;
+        int low=1,high=last;
         while(low<=high){
-            int mid = (low+high)/2;
+            int mid = (low+high)>>1;
+            // calculate time required with given speed..
             int time = calcTime(piles,mid);
-            // System.out.println(mid+" "+time);
+            // if time is less then equals to hours.. it could be potential candidate for the answer..
+            // **potential... so it's not fix.. keep updating it..
             if(time<=H){
-                ans=mid;
+                min = mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return ans;
+        return min;
     }
 }
         
