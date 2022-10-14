4847bab133ed6cf183025c2b8df5f026b6b96167
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-14 12:20:57 +0530

    Time: 9 ms (7.89%), Space: 80 MB (90.81%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -8,45 +8,53 @@ class Solution {
         
         int ans[]=new int[2];
         
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                int check = 1;
-                int element = mat[i][j];
+        int n = row*col;
+        
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int check=1;
+            //i-> row & j-> col
+            int i=mid/col;
+            int j=mid%col;
+            int element = mat[i][j];
                 // check top
                 if(i!=0){
                     if(!(mat[i][j]>mat[i-1][j])){
                         check=0;
-                        continue;
                     }
                 }                
                 // check bottom
                 if(i!=row-1){
                     if(!(mat[i][j]>mat[i+1][j])){
                         check=0;
-                        continue;
                     }
                 }                
                 // check left
                 if(j!=0){
                     if(!(mat[i][j]>mat[i][j-1])){
                         check=0;
-                        continue;
                     }
                 }                
                 // check right
                 if(j!=col-1){
                     if(!(mat[i][j]>mat[i][j+1])){
                         check=0;
-                        continue;
                     }
                 }
                 
                 if(check==1){
                     ans[0]=i;
                     ans[1]=j;
+                    return ans;
                 }
-            }
+                else{
+                    low=low+1;
+                }
+            
         }
+        
+        
         return ans;
     }
 }
