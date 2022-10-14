bdfd98191b22cb87ae60242967c66c4eac11a5ad
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-14 17:30:58 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -37,23 +37,38 @@ class GFG
 //User function Template for Java
 
 class Solution {
-    int median(int matrix[][], int r, int c) {
-        
-        int n = r*c;
-        int res[] = new int[n];
-        for(int i=0;i<n;i++){
-            res[i]=matrix[i/c][i%c];
+    
+    int elementsLessThenMid(int arr[],int element){
+        int n = arr.length;
+        int low = 0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]<=element){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-        
-        Arrays.sort(res);
-        int med=0;
-        // if((n&1)!=1){
-        //     med = (res[n/2]+res[(n+1)/2])/2;
-        // }
-        // else{
-            med = res[n/2];
-        // }
-        
-        return med;
+        return low;
+    }
+    
+    int median(int matrix[][], int r, int c) {
+        int low=1,high=2000;
+        int n=r*c;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int cnt=0;
+            for(int i=0;i<r;i++){
+                cnt+=elementsLessThenMid(matrix[i],mid);
+            }
+            if(cnt<=(n/2)){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
     }
 }
