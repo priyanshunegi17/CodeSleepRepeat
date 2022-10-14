77884830e864b2623cd862b8668e5bb22a4013c6
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-14 17:33:55 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -37,7 +37,7 @@ class GFG
 //User function Template for Java
 
 class Solution {
-    
+    // function to find the number of elements greater then 'element'
     int elementsLessThenMid(int arr[],int element){
         int n = arr.length;
         int low = 0,high=n-1;
@@ -54,14 +54,17 @@ class Solution {
     }
     
     int median(int matrix[][], int r, int c) {
+        // high is upper limit for element within array..
         int low=1,high=2000;
         int n=r*c;
         while(low<=high){
             int mid = (low+high)/2;
             int cnt=0;
+            // count number of elements less then mid...
             for(int i=0;i<r;i++){
                 cnt+=elementsLessThenMid(matrix[i],mid);
             }
+            // median.. which will be greater then n/2 elements...
             if(cnt<=(n/2)){
                 low=mid+1;
             }
