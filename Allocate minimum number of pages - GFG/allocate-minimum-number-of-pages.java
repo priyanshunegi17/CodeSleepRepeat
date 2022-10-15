d1c45b14045aeba8953a74205a097a3921762487
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-15 19:07:20 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.java
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.java
@@ -29,11 +29,12 @@ class GFG {
 // } Driver Code Ends
 
 
-//User function Template for Java
+// https://practice.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+// Using Striver's YT Video..
 
 class Solution 
 {
-    
+    // function: for given mid, check how many groups can be made....
     static boolean calculateM(int arr[],int m,int mid){
         int n = arr.length;
         int count=1,curr=0;
@@ -50,7 +51,7 @@ class Solution
         return count<=m;
     }
     
-    //Function to find minimum number of pages.
+    // Main function...
     public static int findPages(int[]A,int N,int M)
     {
         int min = Integer.MAX_VALUE,max=Integer.MIN_VALUE,sum=0;
@@ -59,21 +60,25 @@ class Solution
             min=i<min?i:min;
             max=i>max?i:max;
         }   
-        
+        // if only 1 group has to be created.. then max will be the sum..
         if(M==1){
             return sum;
         }
+        // if number of groups is equals to size of the array.. then maximum element will be the answer..
         if(M==N){
             return max;
         }
+        //(Edge case..)// if groups are more then N.. it's not possible...
         if(N<M){
             return -1;
         }
-        
+        // search space would be from minimum element upto the sum....
         int low=min,high=sum;
         int ans=-1;
         while(low<=high){
             int mid = (low+high)/2;
+            // if it's true, then try to find minimum value by shifting in left half...
+            // otherwise check in the right half...
             if(calculateM(A,M,mid)){
                 ans=mid;
                 high=mid-1;
