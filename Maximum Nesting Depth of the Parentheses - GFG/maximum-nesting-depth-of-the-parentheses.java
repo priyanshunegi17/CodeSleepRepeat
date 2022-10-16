26918c3fcd7301a63ab6c47956570894abd6399f
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-16 15:21:15 +0530

    Added solution - LeetHub

--- Maximum Nesting Depth of the Parentheses - GFG/maximum-nesting-depth-of-the-parentheses.java
+++ Maximum Nesting Depth of the Parentheses - GFG/maximum-nesting-depth-of-the-parentheses.java
@@ -22,18 +22,20 @@ class GFG {
 
 // } Driver Code Ends
 
+// https://practice.geeksforgeeks.org/problems/maximum-nesting-depth-of-the-parentheses/1
+// ps: it was mentioned that the strings will be always VPS (valid parenthesis string)
 
 class Solution {
     public static int maxDepth(String s) {
         int count=0,curr=0;
         int n=s.length();
-        List<Character> tmp = new ArrayList<>();
         for(int i=0;i<n;i++){
+            // if it's opening parenthesis, increase the count...
             if(s.charAt(i)=='('){
-                // tmp.add('(');
                 curr++;
                 count=curr>count?curr:count;
             }
+            // if it's closing parenthesis, then decrement the count by one...
             else if(s.charAt(i)==')'){
                 curr--;
             }
