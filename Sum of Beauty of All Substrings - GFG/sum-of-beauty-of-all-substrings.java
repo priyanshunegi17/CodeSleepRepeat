417f256e4d2b360f8a3a1340c05a7ab2801871f0
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-17 12:05:55 +0530

    Added solution - LeetHub

--- Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.java
+++ Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.java
@@ -24,8 +24,12 @@ class GFG {
 // } Driver Code Ends
 
 
+// https://practice.geeksforgeeks.org/problems/sum-of-beauty-of-all-substrings-1662962118/1
+// as we know only 26 characters are only there... so better to use array instead of hashmap...
+
+
 class Solution {
-    
+    // return max frequency in the count array..
     static int getMax(int arr[]){
         int max=0;
         for(int i=0;i<arr.length;i++){
@@ -34,9 +38,11 @@ class Solution {
         return max;
     }
     
+    // return min frequency in the count array..
     static int getMin(int arr[]){
         int min=Integer.MAX_VALUE;
         for(int i=0;i<arr.length;i++){
+            // ignore 0, otherwise it'll be taken as minimum... which we don't want...
             if(arr[i]!=0){
                 min=Math.min(min,arr[i]);
             }
@@ -48,9 +54,12 @@ class Solution {
         int n=s.length();
         int ans=0;
         for(int i=0;i<n;i++){
+            // this array will store the count of each character..
             int charCount[] = new int[26];
             for(int j=i;j<n;j++){
+                // increment the count...
                 charCount[s.charAt(j)-'a']+=1;
+                // calculate the beauty,, within the for loop itself....
                 ans+=(getMax(charCount)-getMin(charCount));
             }
         }
