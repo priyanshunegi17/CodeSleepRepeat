10c37e8d4e4c2ee90b81d934227835c9ff3c971b
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-18 13:47:37 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (43.59%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,34 +1,20 @@
 class Solution {
     public String sortSentence(String s) {
-        int n=s.length();
-        int count=1;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)==' '){
-                count++;
-            }
+        String[] str = s.split(" ");
+        int n = str.length;
+        String arr[] = new String[n];
+        for(String st:str){
+            int i = st.charAt(st.length()-1)-'0';
+            arr[i-1]=st.substring(0,st.length()-1);
         }
-        // System.out.println(count);
-        String arr[] = new String[count+1];
-        String temp = "";
-        for(int i=0;i<n;i++){
-            char ch = s.charAt(i);
-            if(s.charAt(i)==' '){
-                temp="";
-            }
-            else if(ch>='1' && ch<='9'){
-                arr[ch-'0']=temp;
-            }
-            else{
-                temp+=ch;
-            }
-        }
-        String res = "";
-        for(int i=1;i<arr.length-1;i++){
-            res+=arr[i];
-            res+=' ';
-        }
-        res+=arr[arr.length-1];
         
-        return res;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<arr.length-1;i++){
+            sb.append(arr[i]).append(" ");
         }
+        sb.append(arr[arr.length-1]);
+        
+        return sb.toString();
+    }
 }
