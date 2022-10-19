ec12b5b54cde4a15590d3c1ecf099a268e9f1273
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-19 18:20:34 +0530

    Added solution - LeetHub

--- Identical Linked Lists - GFG/identical-linked-lists.java
+++ Identical Linked Lists - GFG/identical-linked-lists.java
@@ -70,7 +70,7 @@ class GFG {
 
 // } Driver Code Ends
 
-
+// https://practice.geeksforgeeks.org/problems/identical-linked-lists/1
 /*
 class Node {
     int data;
@@ -87,15 +87,19 @@ class Solution {
     //Function to check whether two linked lists are identical or not.
     public boolean isIdentical (Node head1, Node head2){
         while(head1!=null && head2!=null){
+            // for a given position, check if both both lists are having same data or not..
             if(head1.data!=head2.data){
                 return false;
             }
             head1=head1.next;
             head2=head2.next;
         }
+        // check if both the lists are exhausted or not
         if(head1==null && head2==null){
             return true;
         }
+        // if one of the list is not exhausted.. it means this list is having more nodes..
+        // so this will not be identical..
         return false;
     }
 }
