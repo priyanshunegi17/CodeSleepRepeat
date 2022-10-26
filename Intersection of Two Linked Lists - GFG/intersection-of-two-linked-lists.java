b80062644de7e7a4a00d80b5a1da1d5cd274a5ee
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-10-26 11:16:44 +0530

    Added solution - LeetHub

---
+++ Intersection of Two Linked Lists - GFG/intersection-of-two-linked-lists.java
@@ -0,0 +1,136 @@
+//{ Driver Code Starts
+//
+
+import java.io.*;
+import java.util.*;
+
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        head = tail = new Node(val);
+        
+        size--;
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int n , m;
+            
+            n = sc.nextInt();
+            Node head1 = inputList(n);
+            
+            m = sc.nextInt();
+            Node head2 = inputList(m);
+            
+            Solution obj = new Solution();
+            
+            Node result = obj.findIntersection(head1, head2);
+            
+            printList(result);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* structure of list Node:
+
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+
+*/
+
+class Solution
+{
+    public static Node findIntersection(Node head1, Node head2)
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Node check = head2;
+        int idx=0;
+        while(check!=null){
+            map.put(check.data,++idx);
+            check=check.next;
+        }
+        Node head = new Node(0);
+        Node curr=head;
+        check=head1;
+        while(check!=null){
+            if(map.containsKey(check.data)){
+                Node newNode = new Node(check.data);
+                curr.next=newNode;
+                curr=curr.next;
+            }
+            check=check.next;
+        }
+        return head.next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
