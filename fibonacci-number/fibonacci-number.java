9e070666d832df513b6f10b3c6254d66137a82a6
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-05 19:23:59 +0530

    Time: 1 ms, Memory: 41.7 MB - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,9 +1,13 @@
 class Solution 
 {
+    HashMap<Integer,Integer> cache = new HashMap<>();
     public int fib(int n) {
         if(n<2){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(cache.containsKey(n)) return cache.get(n);
+        int ans= fib(n-1)+fib(n-2);
+        cache.put(n,ans);
+        return ans;
     }
 }
