cc6533c3fd016c15069d52a2a0a0c73f4ec5fd9d
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-08 12:14:21 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (84.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,31 +1,35 @@
 class Solution {
+    
+    int helper(String s, int index, int ans,int n,int sign){
+        if(index>=n) return ans;
+        int digit = s.charAt(index)-'0';
+        if(digit<0 || digit>9) return ans;
+        if(ans>Integer.MAX_VALUE/10 || (ans==Integer.MAX_VALUE/10 && Integer.MAX_VALUE%10<digit)){
+            return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE*-1;
+        }
+        // System.out.println(ans+" ");
+        ans=ans*10+digit;
+        // System.out.println(ans+" ");
+        
+        return helper(s,index+1,ans,n,sign);
+    }
+    
     public int myAtoi(String s) {
-        int index=0,sign=1,total=0;
-        if(s.length()==0) return 0;
+        int index=0,ans=0,sign=1,n=s.length();
         
-        while(index<s.length() && s.charAt(index)==' '){
+        if(n==0) return 0;
+        
+        while(index<n && s.charAt(index)==' '){
             index++;
         }
-        if(index==s.length()) return 0;
-        
+        if(index==n) return 0;
         if(s.charAt(index)=='+' || s.charAt(index)=='-'){
-            sign=s.charAt(index)=='-'?-1:1;
+            sign=s.charAt(index)=='+'?1:-1;
             index++;
         }
+        // System.out.println(s+" "+index);
+        ans = helper(s,index,ans,n,sign);
         
-        while(index<s.length()){
-            int digit = s.charAt(index)-'0';
-            if(digit<0 || digit>9) break;
-            
-            if((total>Integer.MAX_VALUE/10) || ((total == Integer.MAX_VALUE/10) && Integer.MAX_VALUE%10<digit)){
-                return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
-            }
-            
-            total=total*10+digit;
-            index++;
-            
-        }
-        return total*sign;
-        
+        return ans*sign;
     }
 }
