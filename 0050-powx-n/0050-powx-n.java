5b28302d88444668942786a1b72e5c07f9002d1d
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-08 09:41:07 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (91.25%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,18 +1,37 @@
 class Solution {
+    double helper(double x, long n, double ans){
+        if(n==0) return ans;
+        
+        if(n%2==1){
+            ans*=x;
+            n=n-1;
+        }
+        else{
+            x=x*x;
+            n=n/2;
+        }
+        return helper(x,n,ans);
+        
+        
+    }
+    
     public double myPow(double x, int n) {
         double ans=1.0;
         long nn=n;
         if(nn<0) nn=nn*-1;
-        while(nn>0){
-            if(nn%2==1){
-                ans=ans*x;
-                nn=nn-1;
-            }
-            else{
-                x=x*x;
-                nn=nn/2;
-            }
-        }
+        
+        ans = helper(x,nn,ans);
+        
+        // while(nn>0){
+        //     if(nn%2==1){
+        //         ans=ans*x;
+        //         nn=nn-1;
+        //     }
+        //     else{
+        //         x=x*x;
+        //         nn=nn/2;
+        //     }
+        // }
         if(n<0) return (double)1.0/ans;
         return ans;
         
