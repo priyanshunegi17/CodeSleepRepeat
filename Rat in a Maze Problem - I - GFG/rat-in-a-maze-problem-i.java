72f2e9718ca13ff4d3f855762a54e25c2b9d1ef7
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-15 20:27:25 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
@@ -34,27 +34,21 @@ class Rat {
 
 // m is the given matrix and n is the order of matrix
 class Solution {
-    
-    static void helper(int[][] m,int i,int j,int n,String curr,ArrayList<String> ans){
-        // if rat goes beyond boundries ... wrong path
-        if(i<0 || j<0 || i==n || j==n || m[i][j]==0) return;
-        // if cell is already visited.. wrong path
-        if(m[i][j]==-1) return;
-        // if rat reaches the end of the matrix... this path can be included...
+    static void helper(int[][]arr, int i,int j,int n,String curr,ArrayList<String> ans){
+        if(i<0 || j<0 || i>=n || j>=n || arr[i][j]==0 || arr[i][j]==-1) return;
+        
         if(i==n-1 && j==n-1){
             ans.add(new String(curr));
             return;
         }
-        int visited = m[i][j];
-        // mark the visited cell as -1
-        m[i][j]=-1;
-        helper(m,i,j-1,n,curr+'L',ans);
-        helper(m,i,j+1,n,curr+'R',ans);
-        helper(m,i-1,j,n,curr+'U',ans);
-        helper(m,i+1,j,n,curr+'D',ans);
-        m[i][j]=visited;
+        int visited = arr[i][j];
+        arr[i][j]=-1;
+        helper(arr,i,j+1,n,curr+'R',ans);
+        helper(arr,i,j-1,n,curr+'L',ans);
+        helper(arr,i+1,j,n,curr+'D',ans);
+        helper(arr,i-1,j,n,curr+'U',ans);
+        arr[i][j]=visited;
     }
-    
     public static ArrayList<String> findPath(int[][] m, int n) {
         ArrayList<String> ans = new ArrayList<>();
         helper(m,0,0,n,"",ans);
