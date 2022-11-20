7888cc0c4d3e17ddee648bf3ed5929645d0d9367
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-20 13:50:30 +0530

    Time: 17 ms (53.32%), Space: 41.9 MB (29.19%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,58 +1,52 @@
 class Solution {
     
-    boolean helper(char[][] board,int n){
-        
-        boolean flag= false;
+    
+    boolean solve(char[][] board, int n){
         int i=0,j=0;
+        boolean flag=false;
         for(i=0;i<n;i++){
             for(j=0;j<n;j++){
-                if(board[i][j]=='.'){
-                    flag=true;
-                    break;
+                if(board[i][j]=='.'){   
+                flag=true;
+                break;
                 }
             }
-                if(flag) break;
+            if(flag) break;
         }
         if(i==n && j==n) return true;
         
         for(int num=1;num<=n;num++){
-            if(notClash((char)(num+48),i,j,board,n)){
+            if(isValid((char)(num+48),i,j,n,board)){
                 board[i][j]=(char)(num+48);
-                if(helper(board,n))return true;
+                if(solve(board,n)) return true;
                 board[i][j]='.';
-            }    
-        }
+            }
+        }    
         return false;
     }
+    
+    boolean isValid(char ch,int i,int j,int n, char[][]board){
         
-        boolean notClash(char ch,int i,int j,char[][] board,int n){
-            
-            // check for row and column
-            for(int k=0;k<n;k++){
-                if(board[i][k]==ch || board[k][j]==ch) return false;
-            }
-            
-            // starting value of subbox..
-            int s= (int)Math.sqrt(n);
-            int startRow = i - i%s;
-            int startCol = j - j%s;
-            
-            for(int r=startRow;r<startRow+s;r++){
-                for(int c=startCol;c<startCol+s;c++){
-                    if(board[r][c]==ch) return false;
-                }
-            }
-            return true;
+        // check the rows and col
+        for(int k=0;k<n;k++){
+            if(board[i][k]==ch || board[k][j]==ch) return false;
         }
         
-    
-    
+        // check the 3*3 sub-box...
+        int nn =(int)Math.sqrt(n);
+        int startRow = i - i%nn;
+        int startCol = j - j%nn;
+        
+        for(int r=startRow;r<startRow+nn;r++){
+            for(int c=startCol;c<startCol+nn;c++){
+                if(board[r][c]==ch) return false;
+            }
+        }
+        return true;
+    }
     
     public void solveSudoku(char[][] board) {
-        int n = board.length;
-        int total = n*n;
-        
-        helper(board,n);
-        
+        int n =board.length;
+        solve(board,n);
     }
 }
