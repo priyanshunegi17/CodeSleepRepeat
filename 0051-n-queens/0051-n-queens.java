ef6e74fcbbb4939ff905bf02e89ded1e2a54df14
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-20 14:02:27 +0530

    Time: 22 ms (12.80%), Space: 46.8 MB (18.05%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,55 +1,55 @@
 class Solution {
     
-    boolean inbound(int r,int c, int n){
-        if(r<0 || c<0 || r>=n || c>=n) return false;
+    boolean inbound(int i,int j, int n){
+        
+        if(i<0 || j<0 || i>=n || j>=n) return false;
         return true;
     }
     
-    boolean isValid(int r,int c,int n,List<String> curr){
+    boolean isValid(int i, int j, int n, List<String>curr){
         
-        for(int i=0;i<n;i++){
-            if(curr.get(r).charAt(i)=='Q' || curr.get(i).charAt(c)=='Q') return false;
+        for(int k=0;k<n;k++){
+            if(curr.get(i).charAt(k)=='Q' || curr.get(k).charAt(j)=='Q') return false;
         }
         
-        for(int i=0;i<n;i++){
-            if(inbound(r-i,c-i,n) && curr.get(r-i).charAt(c-i)=='Q') return false; 
-            if(inbound(r-i,c+i,n) && curr.get(r-i).charAt(c+i)=='Q') return false; 
-            if(inbound(r+i,c-i,n) && curr.get(r+i).charAt(c-i)=='Q') return false; 
-            if(inbound(r+i,c+i,n) && curr.get(r+i).charAt(c+i)=='Q') return false; 
+        for(int k=0;k<n;k++){
+            if(inbound(i-k,j-k,n) && curr.get(i-k).charAt(j-k)=='Q') return false;
+            if(inbound(i-k,j+k,n) && curr.get(i-k).charAt(j+k)=='Q') return false;
+            if(inbound(i+1,j-k,n) && curr.get(i+1).charAt(j-k)=='Q') return false;
+            if(inbound(i+1,j+k,n) && curr.get(i+1).charAt(j+k)=='Q') return false;
         }
         return true;
     }
     
-    void helper(int row,int n, List<String> curr, List<List<String>> ans){
-    if(row>=n){
-        ans.add(new ArrayList<String>(curr));
-        return;
-    }
-        
-    for(int i=0;i<n;i++){
-        if(isValid(row,i,n,curr)){
-            StringBuilder sb= new StringBuilder(curr.get(row));
-            sb.setCharAt(i,'Q');
-            curr.set(row,sb.toString());
-            helper(row+1,n,curr,ans);
-            sb.setCharAt(i,'.');
-            curr.set(row,sb.toString());
+    void helper(int row, int n, List<String>curr,List<List<String>> ans){
+        if(row==n){
+            ans.add(new ArrayList<>(curr));
+            return;
         }
-    }
         
-        
+        for(int i=0;i<n;i++){
+            if(isValid(row,i,n,curr)){
+                StringBuilder sb = new StringBuilder(curr.get(row));
+                sb.setCharAt(i,'Q');
+                curr.set(row,sb.toString());
+                helper(row+1,n,curr,ans);
+                sb.setCharAt(i,'.');
+                curr.set(row,sb.toString());
+            }
+        }
     }
     
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
-        List<String> curr= new ArrayList<>();
+        List<String> curr = new ArrayList<>();
         for(int i=0;i<n;i++){
-            String s = "";
+            String s="";
             for(int j=0;j<n;j++){
                 s+='.';
             }
             curr.add(s);
         }
+        
         helper(0,n,curr,ans);
         return ans;
     }
