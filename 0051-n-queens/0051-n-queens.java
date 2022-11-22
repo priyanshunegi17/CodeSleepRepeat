a37c3609f776a164c78ed769b0177bcb263d0583
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-22 10:25:15 +0530

    Time: 23 ms (11.82%), Space: 47.3 MB (12.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,26 +1,25 @@
 class Solution {
     
-    boolean inbound(int i, int j, int n){
+    boolean inbound(int i,int j,int n){
         if(i<0 || j<0 || i>=n || j>=n) return false;
         return true;
     }
     
-    boolean notClash(int r, int c, int n, List<String> curr){
-        
+    boolean notClash(int i, int j, int n, List<String> curr){
         for(int k=0;k<n;k++){
-            if(curr.get(r).charAt(k)=='Q' || curr.get(k).charAt(c)=='Q') return false;
+            if(curr.get(i).charAt(k)=='Q' || curr.get(k).charAt(j)=='Q') return false;
         }
         
-        for(int i=0;i<n;i++){
-            if(inbound(r-i,c-i,n) && curr.get(r-i).charAt(c-i)=='Q') return false;
-            if(inbound(r-i,c+i,n) && curr.get(r-i).charAt(c+i)=='Q') return false;
-            if(inbound(r+i,c-i,n) && curr.get(r+i).charAt(c-i)=='Q') return false;
-            if(inbound(r+i,c+i,n) && curr.get(r+i).charAt(c+i)=='Q') return false;
+        for(int k=0;k<n;k++){
+            if(inbound(i-k,j-k,n) && curr.get(i-k).charAt(j-k)=='Q') return false;
+            if(inbound(i-k,j+k,n) && curr.get(i-k).charAt(j+k)=='Q') return false;
+            if(inbound(i+k,j-k,n) && curr.get(i+k).charAt(j-k)=='Q') return false;
+            if(inbound(i+k,j+k,n) && curr.get(i+k).charAt(j+k)=='Q') return false;
         }
         return true;
     }
     
-    void helper(int row, int n, List<String>curr,List<List<String>> ans){
+    void helper(int row,int n, List<String> curr, List<List<String>> ans){
         
         if(row==n){
             ans.add(new ArrayList<>(curr));
@@ -39,19 +38,16 @@ class Solution {
         }
     }
     
-    
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
         List<String> curr = new ArrayList<>();
-        
         for(int i=0;i<n;i++){
-            String s= "";
+            String s="";
             for(int j=0;j<n;j++){
                 s+='.';
             }
             curr.add(s);
         }
-        
         helper(0,n,curr,ans);
         return ans;
     }
