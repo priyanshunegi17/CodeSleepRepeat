165a249b9f4081b4f5aed44abf380a2d5c1958e3
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-24 10:06:24 +0530

    Time: 14 ms (61.56%), Space: 42.1 MB (20.82%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,52 +1,47 @@
 class Solution {
     
-    
-    boolean solve(char[][] board, int n){
-        int i=0,j=0;
-        boolean flag=false;
-        for(i=0;i<n;i++){
-            for(j=0;j<n;j++){
-                if(board[i][j]=='.'){   
-                flag=true;
-                break;
-                }
-            }
-            if(flag) break;
-        }
-        if(i==n && j==n) return true;
-        
-        for(int num=1;num<=n;num++){
-            if(isValid((char)(num+48),i,j,n,board)){
-                board[i][j]=(char)(num+48);
-                if(solve(board,n)) return true;
-                board[i][j]='.';
-            }
-        }    
-        return false;
-    }
-    
-    boolean isValid(char ch,int i,int j,int n, char[][]board){
-        
-        // check the rows and col
+    boolean isValid(int i,int j,int n, char num,char[][] board){
         for(int k=0;k<n;k++){
-            if(board[i][k]==ch || board[k][j]==ch) return false;
+            if(board[i][k]==num || board[k][j]==num) return false;
         }
         
-        // check the 3*3 sub-box...
-        int nn =(int)Math.sqrt(n);
+        int nn = (int)Math.sqrt(n);
         int startRow = i - i%nn;
         int startCol = j - j%nn;
-        
         for(int r=startRow;r<startRow+nn;r++){
             for(int c=startCol;c<startCol+nn;c++){
-                if(board[r][c]==ch) return false;
+                if(board[r][c]==num) return false;
             }
         }
         return true;
     }
     
+    boolean solve(char[][] board,int n){
+        boolean flag=false;
+        int i=0,j=0;
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                if(board[i][j]=='.'){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag) break;
+        }
+        
+        if(i==n && j==n) return true;
+        
+        for(int num=1;num<=n;num++){
+            if(isValid(i,j,n,(char)(num+48),board)){
+                board[i][j]=(char)(num+48);
+                if(solve(board,n)) return true;
+                board[i][j]='.';
+            }
+        }
+        return false;
+    }
+    
     public void solveSudoku(char[][] board) {
-        int n =board.length;
-        solve(board,n);
+        solve(board,board.length);   
     }
 }
