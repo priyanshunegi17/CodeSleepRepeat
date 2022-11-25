ba145fa1d3569a4c9db594e929e62018b13d7a0b
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-25 10:15:03 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (86.73%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,23 +1,21 @@
 class Solution {
-    
-    boolean helper(int[][] image,int i,int j,int m,int n, int check,int color){
-        
-        if(i<0 || j<0 || i>=m || j>=n || image[i][j]!=check || image[i][j]==color) return false;
-        
-        image[i][j]=color;
-        
-        if(helper(image,i,j+1,m,n,check,color)) return true;
-        if(helper(image,i,j-1,m,n,check,color)) return true;
-        if(helper(image,i+1,j,m,n,check,color)) return true;
-        if(helper(image,i-1,j,m,n,check,color)) return true;
-        return false;
-    }
-    
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    boolean helper(int[][]image,int i, int j,int color, int check){
         int m = image.length;
         int n=image[0].length;
-        helper(image,sr,sc,m,n,image[sr][sc],color);
-        return image;
         
+        if(i<0 || j<0 || i>=m || j>=n || image[i][j]==color || image[i][j]!=check) return false;
+        
+        image[i][j]=color;
+        if(helper(image,i,j+1,color,check)) return true;
+        if(helper(image,i,j-1,color,check)) return true;
+        if(helper(image,i+1,j,color,check)) return true;
+        if(helper(image,i-1,j,color,check)) return true;
+        
+        return false;
+        
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        helper(image,sr,sc,color,image[sr][sc]);
+        return image;
     }
 }
