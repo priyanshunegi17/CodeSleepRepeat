6bacbf39291b9b87b4126f5de091926cd73e32ec
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-11-27 19:05:48 +0530

    Time: 26 ms (13.21%), Space: 47.6 MB (15.81%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,27 +1,27 @@
 class Solution {
     
-    int solve(String s,int index,int n, HashSet<String> dict,HashMap<Integer,Integer> map){
+    int helper(HashSet<String> dict,int index,int n, String s,HashMap<Integer,Integer> map){
         if(index==n){
             return 1;
         }
         if(map.containsKey(index)) return map.get(index);
-        String temp = "";
+        String temp="";
         for(int i=index;i<n;i++){
             temp+=s.charAt(i);
-            if(dict.contains(temp) && solve(s,i+1,n,dict,map)>0){
+            if(dict.contains(temp) && helper(dict,i+1,n,s,map)>0){
                 map.put(index,1);
                 return 1;
             }
         }
         map.put(index,0);
         return 0;
+        
     }
     
     public boolean wordBreak(String s, List<String> wordDict) {
-        
-        HashSet<String> dict = new HashSet<>(wordDict);
+        HashSet<String> set = new HashSet<>(wordDict);
         HashMap<Integer,Integer> map = new HashMap<>();
-        return solve(s,0,s.length(),dict,map)>0;
-        
+        if(helper(set,0,s.length(),s,map)>0) return true;
+        return false;
     }
 }
