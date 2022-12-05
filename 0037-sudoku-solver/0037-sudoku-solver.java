262de04dd48322c68f343b733ad6f20aae02a84b
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2022-12-05 11:15:37 +0530

    Time: 10 ms (81.19%), Space: 41.5 MB (61.65%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    boolean isValid(char ch,int i,int j,char[][]board,int n){
+    boolean isValid(char ch,int i,int j,char[][] board,int n){
         for(int k=0;k<n;k++){
             if(board[i][k]==ch || board[k][j]==ch) return false;
         }
@@ -8,6 +8,7 @@ class Solution {
         int nn = (int)Math.sqrt(n);
         int startRow = i - i%nn;
         int startCol = j - j%nn;
+        
         for(int r=startRow;r<startRow+nn;r++){
             for(int c=startCol;c<startCol+nn;c++){
                 if(board[r][c]==ch) return false;
@@ -16,7 +17,7 @@ class Solution {
         return true;
     }
     
-    boolean solve(char[][]board,int n){
+    boolean solve(char[][] board,int n){
         int i=0,j=0;
         boolean flag=false;
         for(i=0;i<n;i++){
@@ -24,41 +25,22 @@ class Solution {
                 if(board[i][j]=='.'){
                     flag=true;
                     break;
-                }       
+                }
             }
             if(flag) break;
         }
-        
         if(i==n && j==n) return true;
-        
         for(int num=1;num<=n;num++){
             if(isValid((char)(num+48),i,j,board,n)){
                 board[i][j]=(char)(num+48);
                 if(solve(board,n)) return true;
                 board[i][j]='.';
-            } 
+            }
         }
         return false;
     }
     
     public void solveSudoku(char[][] board) {
-        solve(board,board.length);
+        solve(board,board.length);   
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
