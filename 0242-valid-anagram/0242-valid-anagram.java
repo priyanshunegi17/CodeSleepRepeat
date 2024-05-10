4cd9320b51162cd96c492536699f1a79787a8be3
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2024-05-10 16:47:46 +0530

    Time: 3 ms (89.27%), Space: 42.8 MB (87.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,20 +3,18 @@ class Solution {
         int n1 = s.length();
         int n2 = t.length();
         if(n1!=n2) return false;
-        
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
+    
+        int[] arr = new int[26];
         for(int i=0;i<n1;i++){
-            int curr = s.charAt(i)-97; 
-            arr1[curr]+=1;
+            int curr = s.charAt(i)-'a';
+            arr[curr]+=1;
         }
         for(int i=0;i<n2;i++){
-            int curr = t.charAt(i)-97; 
-            arr2[curr]+=1;
+            int curr = t.charAt(i)-'a';
+            arr[curr]-=1;
         }
-        for(int i=0;i<n1;i++){
-            int curr = s.charAt(i)-97;
-            if(arr1[curr]!=arr2[curr]){
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
                 return false;
             }
         }
