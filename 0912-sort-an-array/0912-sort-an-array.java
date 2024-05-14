e651166f7fe0a3a6fa1f16ae8d30091617a80415
Priyanshu Negi <48839641+priyanshunegi17@users.noreply.github.com>
2024-05-14 23:40:29 +0530

    Time: 28 ms (53.98%), Space: 55.8 MB (62.46%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,16 +1,18 @@
 class Solution {
     
     public int[] merge(int[] nums, int left, int mid, int right){
+        
         int n1 = mid-left+1;
-        int n2= right - mid;
+        int n2 = right - mid;
+        
         int arr1[] = new int[n1];
         int arr2[] = new int[n2];
         
         for(int i=0;i<n1;i++){
             arr1[i]=nums[left+i];
         }
-        for(int i=0;i<n2;i++){
-            arr2[i]=nums[mid+i+1];
+        for(int j=0;j<n2;j++){
+            arr2[j]=nums[mid+j+1];
         }
         
         int i=0,j=0,k=left;
@@ -26,28 +28,34 @@ class Solution {
             }
             k++;
         }
+        
         while(i<n1){
             nums[k++]=arr1[i++];
         }
         while(j<n2){
             nums[k++]=arr2[j++];
         }
+        
         return nums;
-    }
-    
-    public int[] mergeSort(int []nums, int l, int r){
-        if(l>=r){
-            return nums;
-        }
-        int mid = (l+r)/2;
-        mergeSort(nums,l,mid);
-        mergeSort(nums,mid+1,r);
-        return merge(nums,l,mid,r);
+        
+        
         
     }
     
+    
+    public int[] mergeSort(int []nums, int left, int right){
+        if(left<right){
+            int mid = (left+right)/2;
+            mergeSort(nums,left,mid);
+            mergeSort(nums,mid+1,right);
+            return merge(nums,left,mid,right);
+        }
+        return nums;
+    }
+    
+    
     public int[] sortArray(int[] nums) {
         int n = nums.length;
-        return mergeSort(nums,0,n-1);
+        return mergeSort(nums,0,n-1);    
     }
 }
